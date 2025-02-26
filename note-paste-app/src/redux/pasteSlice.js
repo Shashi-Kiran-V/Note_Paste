0a7b7921705cad92e4d612597f6840b5497fec8e
Shashi-Kiran-V <shashikiranshetty02@gmail.com>
2025-02-26 18:56:43 +0530

    pasteSlice updated

--- note-paste-app/src/redux/pasteSlice.js
+++ note-paste-app/src/redux/pasteSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { act } from 'react';
 import toast from 'react-hot-toast';
 const initialState = {
   pastes: (() => {
@@ -24,13 +25,30 @@ export const pasteSlice = createSlice({
      toast("paste created successfully");
     },
     updateToPastes: (state,action) => {
-      
+      const paste=action.payload;
+      const index=state.pastes.findIndex((item)=> item._id===paste._id)
+      if(index>=0){
+        state.pastes[index]=paste;
+        localStorage.setItem("pastes",JSON.stringify(state.pastes));
+        toast.success("paste updated");
+      }
     },
    resetAllPastes: (state, action) => {
+    state.pastes=[];
+    localStorage.removeItem("pastes");
       
     },
     removeFromPastes:(state,action)=>{
+      const pasteId=action.payload;
+      console.log(pasteId);
+      const index=state.pastes.findIndex((item)=>item._id===pasteId)
 
+      if(index>=0){
+        state.pastes.splice(index,1);
+        localStorage.setItem("pastes",JSON.stringify(state.pastes));
+        toast.success("Paste Deleted")
+      }
+      
     },
   },
 })
