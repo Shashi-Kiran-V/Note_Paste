8039f2a892d02f210859e79a264270f2c06e4f80
Shashi-Kiran-V <shashikiranshetty02@gmail.com>
2025-02-26 17:41:42 +0530

    placeholder added

--- note-paste-app/src/components/Home.jsx
+++ note-paste-app/src/components/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useSearchParams } from 'react-router-dom';
 const Home = () => {
+  const [title,setTitle]=useState('');
+  const [value,setValue]=useState('');
+  const [searchParams,setSearchParams]=useSearchParams();
+  const pasteId=searchParams.get("pasteId");
   return (
     <div>
-      Home
+      <input 
+      className='p-2 rounded-2xl mt-2'
+       type="text"
+       placeholder="enter title here"
+      value={title}
+      onChange={(e)=>setTitle(e.target.title)}
+      />
+      <button>
+        {
+          pasteId ? "Update Paste" : "Create my Paste"
+        }
+
+      </button>
     </div>
   )
 }
