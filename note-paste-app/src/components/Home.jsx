1df2472a41fd0b9734dda95b49375a15446ab2d1
Shashi-Kiran-V <shashikiranshetty02@gmail.com>
2025-02-27 15:18:47 +0530

    borders updated

--- note-paste-app/src/components/Home.jsx
+++ note-paste-app/src/components/Home.jsx
@@ -42,7 +42,7 @@ const Home = () => {
     <div>
       <div className='flex flex-row gap-7 place-content-between'>
       <input 
-      className='p-2 rounded-2xl mt-2 w-[66%] pl-4'
+      className='p-2 rounded-2xl mt-2 w-[66%] pl-4 border border-gray-300'
        type="text"
        placeholder="enter title here"
       value={title}
@@ -57,7 +57,7 @@ const Home = () => {
     </div>
     <div className='mt-8'>
       <textarea 
-      className="rounded-2xl mt-4, min-w-[500px] p-4"
+      className="rounded-2xl mt-4, min-w-[500px] p-4 border border-gray-300"
         value={value}
         placeholder="enter content here"
         onChange={(e)=> setValue(e.target.value)}
--- note-paste-app/src/components/ViewPaste.jsx
+++ note-paste-app/src/components/ViewPaste.jsx
@@ -12,7 +12,7 @@ const ViewPaste = () => {
     <div>
       <div className='flex flex-row gap-7 place-content-between'>
       <input 
-      className='p-2 rounded-2xl mt-2 w-[66%] pl-4'
+      className='p-2 rounded-2xl mt-2 w-[66%] pl-4 border border-gray-300'
        type="text"
        placeholder="enter title here"
       value={paste.title}
@@ -28,7 +28,7 @@ const ViewPaste = () => {
     </div>
     <div className='mt-8'>
       <textarea 
-      className="rounded-2xl mt-4, min-w-[500px] p-4 border-color: var(--color-cyan-800);"
+      className="rounded-2xl mt-4, min-w-[500px] p-4 border border-gray-300"
         value={paste.content}
         placeholder="enter content here"
         disabled
