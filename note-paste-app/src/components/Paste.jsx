12425fd03841548be634ee8c998daac78785e62b
Shashi-Kiran-V <shashikiranshetty02@gmail.com>
2025-02-27 14:20:42 +0530

    added delete view edit and copy buttons to the notes card

--- note-paste-app/src/components/Paste.jsx
+++ note-paste-app/src/components/Paste.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { removeFromPastes } from '../redux/pasteSlice';
 
 const Paste = () => {
   const pastes=useSelector((state)=>state.paste.pastes);
@@ -7,7 +8,9 @@ const Paste = () => {
   const dispatch=useDispatch();
   const [searchTerm,setSearchTerm]=useState('');
   const filterData=pastes.filter((paste)=>paste.title.toLowerCase().includes(searchTerm.toLocaleLowerCase()));
-
+function handleDelete(pasteId){
+  dispatch(removeFromPastes(pasteId))
+}
   
   return (
     <div>
@@ -19,14 +22,39 @@ const Paste = () => {
       onChange={(e)=> setSearchTerm(e.target.value)}
       
       />
-      <div className='flex flex-col gap-5'>
+      <div className='flex flex-col gap-5 mt-5'>
         {
           filterData.length>0 && 
           filterData.map((paste)=>{
               return (
 
-                <div>
-                  {paste.title}
+                <div className='border'>
+                  <div>
+                    {paste.title}
+                    </div>
+                    <div>
+                    {paste.content}
+                    </div> 
+                    <div className='flex flex-row gap-4 place-content-evenly'>
+                      <button>
+                        Edit
+                      </button>
+                      <button>
+                        View
+                      </button>
+                      <button onClick={handleDelete}>
+                        Delete
+                      </button>
+                      <button>
+                        Copy
+                      </button>
+                      <button>
+                        Share
+                      </button>
+                      </div>
+                      <div>
+                        {paste.createdAt}
+                        </div>
                   </div>
               )
 
